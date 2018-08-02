83d166ed59a09f6d6da88e9163af2d418877e1b1
Karla R <karla.y.reyes12@gmail.com>
2018-08-02 09:54:06 +0000

    automatically backed up by learn

--- db/migrate/20180802095240_create first actors table.rb
+++ db/migrate/20180802095240_create first actors table.rb
@@ -1,5 +1,5 @@
 class CreateFirstActorsTable > ActiveRecord::Migration
   def change
     create_table :actors do |t|
-      t.string :name
-      t.integer :show_id
+      t.string :first_name
+      t.string :last_name
